3061628cab05824d0feac529999cd1de66ae22ba
Mitchell Budge <mitchellbudge@gmail.com>
2019-09-16 19:44:01 -0600

    Add file via upload

---
+++ FliptheBool.playground/Contents.swift
@@ -0,0 +1,15 @@
+/*
+ Create a function that reverses a boolean value.
+ 
+ Examples
+ reverse(true) ➞ false
+ 
+ reverse(false) ➞ true
+*/
+
+func reverse(_ bool: Bool) -> Bool {
+    return !bool
+}
+
+reverse(true)
+reverse(false)
