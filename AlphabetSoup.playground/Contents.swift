b5d9c0682cc5573217735a56dffb829d66cbbc48
Mitchell Budge <mitchellbudge@gmail.com>
2019-09-19 12:02:14 -0600

    Add incomplete challenge via upload

---
+++ AlphabetSoup.playground/Contents.swift
@@ -0,0 +1,20 @@
+/*
+ Alphabet Soup
+ Create a function that takes a string and returns a string with its letters in alphabetical order.
+ 
+ Examples
+ alphabetSoup("hello") ➞ "ehllo"
+ 
+ alphabetSoup("edabit") ➞ "abdeit"
+ 
+ alphabetSoup("hacker") ➞ "acehkr"
+ 
+ alphabetSoup("geek") ➞ "eegk"
+ 
+ alphabetSoup("javascript") ➞ "aacijprstv"
+*/
+
+func alphabetSoup(_ str: String) -> String {
+    // solution replaced in Swift 5
+}
+
