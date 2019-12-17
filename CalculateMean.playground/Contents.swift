43afcc765db79344dac797d22aa108d53daf5aae
Mitchell Budge <mitchellbudge@gmail.com>
2019-12-17 11:11:08 -0700

    Update solution

--- CalculateMean.playground/Contents.swift
+++ CalculateMean.playground/Contents.swift
@@ -1,7 +1,5 @@
 import UIKit
 
-var str = "Hello, playground"
-
 func mean(_ arr: [Int]) -> Double {
     return (Double(arr.reduce(0, +))/Double(arr.count))
 }
@@ -11,10 +9,4 @@ mean([1, 0, 4, 5, 2, 4, 1, 2, 3, 3, 3])
 mean([2, 3, 2, 3])
 
 mean([3, 3, 3, 3, 3])
-
-
-let array = [1, 2, 4, 5, 10]
-let total = array.reduce(0) { (<#Result#>, <#Int#>) -> Result in
-    <#code#>
-}
 
